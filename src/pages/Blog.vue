27991c16f5066ca6b53ea3a57545fea20271dea7
MichalKozlowski2017 <m.kozlowski87@gmail.com>
2025-01-23 17:19:04 +0100

    added blog page

---
+++ src/pages/Blog.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+    <h1>BLOG - TEST</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Blog",
+    titleTemplate: "",
+    script: [
+      {
+        innerHTML:
+          "gtag('event', 'conversion', {'send_to': 'AW-11238080681/0opNCOaZ-LYYEKmB3u4p'}); ",
+      },
+    ],
+    meta: [
+      { name: "author", content: "Jerzy Nowak" },
+      {
+        name: "description",
+        content:
+          "blog firmy budowlanej Roycon - sprawdź nasze realizacje, opinie klientów i ciekawostki ze świata budownictwa",
+      },
+      {
+        name: "keywords",
+        content:
+          "firma budowlana jelenia góra, firma budowlana Wałbrzych, firma budowlana legnica, generalny wykonawca jelenia góra, generalny wykonawca Wałbrzych, generalny wykonawca Legnica, wykonawca izodom 2000, hale stalowe jelenia góra, hale stalowe Wałbrzych, hale stalowe legnica, hale stalowe astron, domy zeroenergetyczne, budynek zeroenergetyczny",
+      },
+    ],
+  },
+  components: {},
+};
+</script>
